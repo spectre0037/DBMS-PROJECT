a806d2032cdb2332609f4cc77c1de50696070080
Abdullah-crns <u2023037@giki.edu.pk>
2025-03-01 05:37:17 +0500

    x

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,10 +9,10 @@ const matchers = Object.keys(routeAccessMap).map((route) => ({
 
 console.log(matchers);
 
-export default clerkMiddleware((auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
   // if (isProtectedRoute(req)) auth().protect()
 
-  const { sessionClaims } = auth();
+  const { sessionClaims } = await auth();
 
   const role = (sessionClaims?.metadata as { role?: string })?.role;
 
